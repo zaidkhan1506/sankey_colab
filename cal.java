e10a5f5b08d93f88c9d0c9d306d064eaefe23918
Nikhil Maurya <96909757+the-nikhilmaurya@users.noreply.github.com>
2023-07-11 15:36:55 +0530

    Update cal.java

--- cal.java
+++ cal.java
@@ -1,12 +1,12 @@
 import java.util.*;
 public class cal{
 public static void main (String args[]){
-Addition a = new addition();
 Scanner s = new Scanner(System.in);
 int b=s.nextInt();
 int c = s.nextInt();
 
-  System.out.println(c.add(b,c));
+  Nikhil n = new Nikhil();
+  System.out.println(n.add(b,c));
 Abhay s = new Abhay();
 
   System.out.println(s.subtract(b,c));
