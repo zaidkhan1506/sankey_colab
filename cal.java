1ff9a7f9f419f6bb9d951137f70c4f50b52ffacb
zaidkhan1506 <zk972631@gmail.com>
2023-07-11 15:25:54 +0530

    main file

---
+++ cal.java
@@ -0,0 +1,20 @@
+import java.utils.*;
+public class cal{
+public static void main (String args[]){
+Addition a = new addition();
+Scanner s = new Scanner(System.in);
+int b=s.nextInt();
+int c = s.nextInt();
+c.add(b,c);
+Abhay s = new Abhay();
+s.subtract(b,c);
+Shree  m = new shree(); 
+m.multiply(b,c);
+Jiggar j = new Jiggar();
+j.mod(b,c);
+Nihar h =new Nihar();
+h.div();
+}
+}
+
+
